f2932f28b1f5a7a098cb0498e47fdf41d91f6559
Szymon-Budziak <szymonbudziak@gmail.com>
2023-01-31 23:41:29 +0100

    Displaying logged in users on frontpage

--- webchat/core/views.py
+++ webchat/core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.sessions.models import Session
+from django.contrib.auth.models import User
+from django.utils import timezone
 from .forms import SignUpForm
 
 
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    users = get_logged_in_users()
+    return render(request, 'core/frontpage.html', {'users': users})
 
 
 def signup(request):
@@ -17,3 +21,14 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'core/signup.html', {'form': form})
+
+
+def get_logged_in_users():
+    sessions = Session.objects.filter(expire_date__gte=timezone.now())
+    uid_list = []
+    for session in sessions:
+        data = session.get_decoded()
+        uid = data.get('_auth_user_id', None)
+        if uid:
+            uid_list.append(uid)
+    return User.objects.filter(id__in=uid_list)
